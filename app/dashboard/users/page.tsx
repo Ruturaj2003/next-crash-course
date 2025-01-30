281d1982477700ec92da4944cb3931e304b9a6ec
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-01-30 19:25:34 +0530

    Add user links to dashboard and create dynamic user page

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,4 +1,23 @@
+import Link from 'next/link';
+
 function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <ul className="text-4xl">
+        <li>
+          <Link href={'/dashboard/users/1'}>User 1 </Link>
+        </li>
+        <li>
+          <Link href={'/dashboard/users/2'}>User 2 </Link>
+        </li>
+        <li>
+          <Link href={'/dashboard/users/3'}>User 3 </Link>
+        </li>
+        <li>
+          <Link href={'/dashboard/users/4'}>User 4 </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 export default page;
